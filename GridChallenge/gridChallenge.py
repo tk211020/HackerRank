d9ab39c025e5a07e310007556b8a3550c6d269fb
Bamboo <bamboo@Bamboos-MacBook-Pro.local>
2022-11-24 15:02:52 +0800

    Problem: Grid Challenge, Language: Python.

---
+++ GridChallenge/gridChallenge.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gridChallenge' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING_ARRAY grid as parameter.
+#
+
+def gridChallenge(grid):
+    # Write your code here
+    last = ""
+    for i in range(len(grid)):
+        if i != 0: 
+            current = sorted(grid[i])
+            for j in range(len(current)):
+                if last[j]>current[j]: return "NO"
+        last = sorted(grid[i])
+    return "YES"
+    return grid
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        grid = []
+
+        for _ in range(n):
+            grid_item = input()
+            grid.append(grid_item)
+
+        result = gridChallenge(grid)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
+
