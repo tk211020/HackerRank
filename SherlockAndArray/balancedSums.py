04ad0c3bae2523c6344f9132882ee1a2b84da959
Bamboo <bamboo@Bamboos-MacBook-Pro.local>
2022-11-25 16:31:01 +0800

    Problem: Sherlock and Array, Language: Python.

---
+++ SherlockAndArray/balancedSums.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'balancedSums' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def balancedSums(arr):
+    # Write your code here
+    leftSum, rightSum = 0, sum(arr)
+    
+    for i in range(len(arr)):
+        rightSum = rightSum - arr[i]
+        if leftSum == rightSum :
+            return "YES"
+        leftSum = leftSum + arr[i]
+    return "NO"
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    T = int(input().strip())
+
+    for T_itr in range(T):
+        n = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = balancedSums(arr)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
+
