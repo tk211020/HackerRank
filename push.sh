dd55d9ac89c5b29adec78b27077b0d297a466891
Bamboo <bamboo@Bamboos-MacBook-Pro.local>
2022-12-08 08:40:05 +0800

    Update push.sh

--- push.sh
+++ push.sh
@@ -8,8 +8,18 @@ mkdir $dirName
 mv code $2
 mv $2 $dirName
 
+if [ "$3" = "py" ]
+then
+        lan="Python"
+elif [ "$3" = "js" ]
+then
+        lan="JavaScript"
+else
+        lan=$3
+fi
+
 git pull
 git add $dirName
-git commit -m "Problem: $1, Language: Python."
+git commit -m "Problem: $1, Language: $3."
 git push
 
