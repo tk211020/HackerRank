71119cd6db4c9f2dd342bf0a0e6ae6c9d031d13d
Bamboo <bamboo@Bamboos-MacBook-Pro.local>
2022-12-09 16:58:04 +0800

    Problem: Reverse a doubly linked list, Language: JavaScript.

---
+++ LinkedList/reverseDLL.js
@@ -0,0 +1,121 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+const DoublyLinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+        this.prev = null;
+    }
+};
+
+const DoublyLinkedList = class {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    insertNode(nodeData) {
+        let node = new DoublyLinkedListNode(nodeData);
+
+        if (this.head == null) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+            node.prev = this.tail;
+        }
+
+        this.tail = node;
+    }
+};
+
+function printDoublyLinkedList(node, sep, ws) {
+    while (node != null) {
+        ws.write(String(node.data));
+
+        node = node.next;
+
+        if (node != null) {
+            ws.write(sep);
+        }
+    }
+}
+
+/*
+ * Complete the 'reverse' function below.
+ *
+ * The function is expected to return an INTEGER_DOUBLY_LINKED_LIST.
+ * The function accepts INTEGER_DOUBLY_LINKED_LIST llist as parameter.
+ */
+
+/*
+ * For your reference:
+ *
+ * DoublyLinkedListNode {
+ *     int data;
+ *     DoublyLinkedListNode next;
+ *     DoublyLinkedListNode prev;
+ * }
+ *
+ */
+
+function reverse(llist) {
+    // Write your code here
+    
+    while(llist){
+        let tmp = llist.next;
+        llist.next = llist.prev;
+        llist.prev = tmp;
+        if(tmp == null) break;
+        llist = tmp;
+    }
+    return llist
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const t = parseInt(readLine(), 10);
+
+    for (let tItr = 0; tItr < t; tItr++) {
+        const llistCount = parseInt(readLine(), 10);
+
+        let llist = new DoublyLinkedList();
+
+        for (let i = 0; i < llistCount; i++) {
+            const llistItem = parseInt(readLine(), 10);
+            llist.insertNode(llistItem);
+        }
+
+        let llist1 = reverse(llist.head);
+
+        printDoublyLinkedList(llist1, " ", ws)
+        ws.write("\n");
+    }
+
+    ws.end();
+}
+
